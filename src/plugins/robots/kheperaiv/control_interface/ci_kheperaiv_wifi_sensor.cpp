a5d14db2315509969228165151c5e3a5be3f2d28
Khai Yi Chin <khaiyichin@gmail.com>
2025-02-23 23:25:50 -0500

    Added WiFi communication classes for real robots.

---
+++ src/plugins/robots/kheperaiv/control_interface/ci_kheperaiv_wifi_sensor.cpp
@@ -0,0 +1,26 @@
+#include "ci_kheperaiv_wifi_sensor.h"
+
+namespace argos {
+
+    /****************************************/
+    /****************************************/
+
+#ifdef ARGOS_WITH_LUA
+    void CCI_KheperaIVWiFiSensor::CreateLuaState(lua_State* pt_lua_state) {
+        // TODO
+    }
+#endif
+
+    /****************************************/
+    /****************************************/
+
+#ifdef ARGOS_WITH_LUA
+    void CCI_KheperaIVWiFiSensor::ReadingsToLuaState(lua_State* pt_lua_state) {
+        // TODO
+    }
+#endif
+
+    /****************************************/
+    /****************************************/
+
+}
